80a5f459d20671ad1132024a9892218d5c341a5f
Christian Tracy <christian.tracy@nan-labs.com>
2019-01-03 10:55:46 -0300

    Prism and css configuration added

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-export default () => <div>Hello</div>
+import Prism from 'prismjs'
+
+var code = `
+  const example = "This is an example"
+  const test = () => {
+    console.log(example)
+  }
+`
+var html = Prism.highlight(code, Prism.languages.javascript, 'javascript')
+
+export default () => (
+  <div>
+    Code
+    <pre className='line-numbers language-javascript' data-start='0'>
+      <code className='language-javascript' dangerouslySetInnerHTML={{ __html: html }} />
+    </pre>
+  </div>
+)
